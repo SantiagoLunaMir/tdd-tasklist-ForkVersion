1cb91cc2f62b501748354630dffad2253ce1f651
Santiago Luna Mireles <a335833@alumnos.uaslp.mx>
2023-05-04 18:17:44 -0600

    TaskListTest listo

--- src/main/java/edu/uaslp/objetos/taskslist/TaskList.java
+++ src/main/java/edu/uaslp/objetos/taskslist/TaskList.java
@@ -1,9 +1,66 @@
 package edu.uaslp.objetos.taskslist;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class TaskList{
-    private LinkedList<Task> listaTareas;
+    LinkedList<Task> taskList;
+    TaskList(){
+     taskList=new LinkedList<>();
+    }
+    public int getSize(){
+        return taskList.size();
+    }
+    public void add(Task task){
+        taskList.add(task);
+    }
+    public void remove(Task task){
+        taskList.remove(task);
+    }
+    public Task find(String title) throws TaskNotFoundException{
+        for(Task task:taskList){
+            if(task.getTitle().equals(title)){
+                return task;
+            }
+        }
+        throw new TaskNotFoundException("Task with title '"+title+"' not found");
+    }
+    public void markAsDone(String title){
+        Task task=find(title);
+        task.setDone(true);
+    }
+    public void markAsNotDone(String title){
+        Task task=find(title);
+        task.setDone(false);
+    }
+    public Task getNextTask(){
+        Task nextTask;
+        Iterator<Task> iterator=taskList.iterator();
+        nextTask=iterator.next();
+        while(iterator.hasNext()){
+            Task actualTask= iterator.next();
+            if(actualTask.getDueDate().isBefore(nextTask.getDueDate())&&!actualTask.isDone()){
+                nextTask=actualTask;
+            }
+        }
+        return nextTask;
+    }
+    public List getNextTasks(){
+        LinkedList<Task> taskFound=new LinkedList<>();
+        Iterator<Task> iterator=taskList.iterator();
+        while(iterator.hasNext()){
+            Task actualTask= iterator.next();
+            if(actualTask.getDueDate().isAfter(LocalDateTime.now()) && !actualTask.isDone()){
+                Task task=new Task(actualTask.getTitle(), actualTask.getDescription(),actualTask.getDueDate() , actualTask.isDone());
+                taskFound.add(task);
+            }
+        }
+        taskFound.sort((Task a, Task b)->a.getDueDate().compareTo(b.getDueDate()));
+        return taskFound;
+    }
+
+    /*
+      private LinkedList<Task> listaTareas;
     private int size=0;
     public TaskList(){
         listaTareas=new LinkedList<Task>();
@@ -53,4 +110,5 @@ public class TaskList{
     public LinkedList<Task> getNextTasks(){
         return null;
     }
+     */
 }
--- src/main/java/edu/uaslp/objetos/taskslist/TaskNotFoundException.java
+++ src/main/java/edu/uaslp/objetos/taskslist/TaskNotFoundException.java
@@ -1,11 +1,11 @@
 package edu.uaslp.objetos.taskslist;
 
 public class TaskNotFoundException extends RuntimeException{
-
-    private String str1 = "Task with title '";
-    private String str3 = "' not found";
+    public TaskNotFoundException(String message){
+       super(message);
+    }
     public TaskNotFoundException(){
-        super("Task with title 'Hacer ejercicio' not found");
+
     }
 
 }
